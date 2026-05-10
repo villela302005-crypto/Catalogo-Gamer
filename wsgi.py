7ebaa12a5635222d35d8a85b2f6896d41d1066c4
villela302005-crypto <villela302005@gmail.com>
2026-05-09 20:27:11 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-
-# Asegúrate de que aquí diga 'settings' (sin tildes)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
 application = get_wsgi_application()
