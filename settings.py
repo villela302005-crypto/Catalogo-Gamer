eb7a8cdff84b8a83b76a709a7ac159dda08f24d9
villela302005-crypto <villela302005@gmail.com>
2026-05-09 19:50:03 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-97ef!1x=jgw&_+(h(wvuwy15-^e+tj*6ds-m62lm4l3hn*tgq!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
